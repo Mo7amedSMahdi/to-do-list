8eb07a97e962630df3e3ac0a7439bd4cfabc06df
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-03-31 00:30:54 +0300

    Implement Add, Edit and Remove task functionalities using ES6 syntax.
    - Create modules folder.
    - Create task class in index.js.
    - Create form.js to get the user tasks.
    - Create functionalities.js to implement Add, Edit, Remove task functionalities.
    - Save and retrive tasks from local storage.

--- src/index.js
+++ src/index.js
@@ -1,55 +1,37 @@
 import './style.css';
+import Form from './modules/form.js';
 
-const listContainer = document.querySelector('.nav--list');
-const tasks = [
-  { index: 0, description: 'learn more about webpack', completed: true },
-  { index: 1, description: 'learn more about javascript', completed: false },
-  { index: 2, description: 'use webpack in a project', completed: true },
-  {
-    index: 3,
-    description: 'create to-do list for my learning objectives',
-    completed: false,
-  },
-  { index: 4, description: 'lear react', completed: false },
-];
+import {
+  addTask as Add,
+  editTasks as Edit,
+  deletTask as Delete,
+} from './modules/functionalities.js';
 
-const renderTasks = () => {
-  let list = '';
-  tasks.forEach((task) => {
-    list += `<li>
-                    <div class="list__item">
-                        <input type="checkbox" value="${task.completed}">
-                        <p>
-                            ${task.description}
-                        </p>
-                        <button type="button"  class="btn" data-id="${task.id}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"
-                                id="three-dots">
-                                <g id="_20x20_three-dots--grey" data-name="20x20/three-dots--grey"
-                                    transform="translate(24) rotate(90)">
-                                    <rect id="Rectangle" width="24" height="24" fill="none" />
-                                    <circle id="Oval" cx="1" cy="1" r="1" transform="translate(5 11)" stroke="#000"
-                                        stroke-miterlimit="10" stroke-width="0.5" />
-                                    <circle id="Oval-2" data-name="Oval" cx="1" cy="1" r="1"
-                                        transform="translate(11 11)" stroke="#000" stroke-miterlimit="10"
-                                        stroke-width="0.5" />
-                                    <circle id="Oval-3" data-name="Oval" cx="1" cy="1" r="1"
-                                        transform="translate(17 11)" stroke="#000" stroke-miterlimit="10"
-                                        stroke-width="0.5" />
-                                </g>
-                            </svg>
-                        </button>
-                    </div>
-                </li>`;
-  });
+class Task {
+  constructor(taskArray) {
+    this.taskArray = taskArray;
+  }
 
-  listContainer.innerHTML = list;
-  const checkBox = listContainer.querySelectorAll('input[type="checkbox"]');
-  checkBox.forEach((item) => {
-    if (item.value === 'true') {
-      item.checked = true;
+  render = () => {
+    Form(this.addTask, this.taskArray);
+
+    Add(this.taskArray);
+
+    Edit(this.taskArray, this.addTask);
+    Delete(this.taskArray, this.addTask);
+  };
+
+  addTask = (task) => {
+    if (task) {
+      this.taskArray.push(task);
+      localStorage.setItem('TASKS', JSON.stringify(this.taskArray));
+      this.render();
+    } else {
+      localStorage.setItem('TASKS', JSON.stringify(this.taskArray));
     }
-  });
-};
+  };
+}
 
-document.addEventListener('DOMContentLoaded', renderTasks);
+const task = new Task(JSON.parse(localStorage.getItem('TASKS') || []));
+task.render();
+task.addTask();
