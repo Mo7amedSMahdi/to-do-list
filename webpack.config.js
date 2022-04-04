ebd1405810b409aefcd59205569aff5ee60adb4b
Mohammed Salah Mahdi <Mo7ammed.salah@outlook.com>
2022-04-04 10:10:18 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/todo-list/',
+    publicPath: '/to-do-list/',
     clean: true,
   },
 };
